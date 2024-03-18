91d650c973a39eca9f97ce857f38c53e1e97a486
Aliz Gosztola <aliz.gosztola@gmail.com>
2024-03-17 23:11:44 -0400

    test

--- vue/src/components/SearchBarComponent.vue
+++ vue/src/components/SearchBarComponent.vue
@@ -117,7 +117,7 @@ export default {
         if (response.status === 200) {
           const relativeImagePath = response.data;
         // Construct absolute image URL by appending relative path to base URL
-        this.imgPath = `https://wyrmspandragons.netlify.app${relativeImagePath}`;
+        this.imgPath = `https://wyrmspandragons.netlify.app/${relativeImagePath}`;
           // this.imgPath = "../../";
           // this.imgPath += response.data;
         }
