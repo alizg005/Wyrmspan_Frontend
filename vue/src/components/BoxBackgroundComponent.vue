6cf4f33cf2a4bac070c1de8bdd7d33c98081fcb2
Aliz Gosztola <aliz.gosztola@gmail.com>
2024-03-19 21:25:45 -0400

    efere

--- vue/src/components/BoxBackgroundComponent.vue
+++ vue/src/components/BoxBackgroundComponent.vue
@@ -47,7 +47,7 @@ export default {
     computed: {
         dragonImgHTML() {
             if (this.imgPath) {
-                return `<img class="dragon-image" src="${this.imgPath}" alt="${this.dragonProp.name}" style="height: 250px;">`;
+                return `<div class="dragon-image">${this.imgPath} style="height: 250px; </div>`;
             } else {
                 return '';
             }
